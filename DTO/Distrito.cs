a8fd7d2ac95bbd3ff4800ab2df848e97457599e6
kendra-svg <ksanchov@ucenfotec.ac.cr>
2024-03-22 05:18:34 -0600

    Se define UsuarioBase y Cita Se crea la clase de Pago Se crea la clase de Ubicacion Se crea la clase de Provincia Se crea la clase de Canton Se crea la clase de Distrito Se crea la clase de Sede Se crea la clase de Paciente

---
+++ DTO/Distrito.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Distrito
+    {
+        public string Id { get; set; } //Vendria a ser el id_distritos del diagrama de la base de datos
+        public string Nombre { get; set; } //Vendria a ser el nombre_distritos del diagrama de la base de datos
+    }
+}
---
+++ DTO/Ubicacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Ubicacion
+    {
+        public string Coordenadas { get; set; }
+        public string Id { get; set; } //Vendria a ser el id_coordenadas del diagrama de la base de datos
+
+        public int IdSedes { get; set; } //Vendria a ser el sedes_id_sedes del diagrama de la base de datos
+    }
+}
