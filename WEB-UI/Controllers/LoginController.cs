d575727d7530a4061f465dfe0fae1017db33ccce
Vanessa <vvillegast@ucenfotec.ac.cr>
2024-04-01 10:10:28 -0600

    Login y Registro. Para subir el registro tendria que subir tambien el login, el caso es que el css de login no me lo trae, no hay errores, solamente por el momento no se vera el css.

---
+++ WEB-UI/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UsuarioBase usuario)
+        {
+            if (usuario.Nombre == null || usuario.contrasennas == null)
+            {
+                ViewBag.Message = "Usuario y/o password incorrectos";
+                return View();
+            }
+
+            //httpClient -> API(user) --> Admin --> DAO --> BD
+
+            //cambiar por nombres y apellidos
+            usuario.Nombre = "Juan Perez Perez";
+
+            //Inicia sesion, propiedades de la clase usuario. Nombre completo no existe, cambiar por nombres  y apellidos
+            HttpContext.Session.SetString("user", usuario.Nombre);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
