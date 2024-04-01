b0ae119f75a50a47ebec6c2df18859d5f29e0f39
fernandov24 <137198074+fernandov24@users.noreply.github.com>
2024-04-01 11:02:17 -0600

    Listar crear detalles en examen personal del paciente

--- WEB-UI/Controllers/PacienteController.cs
+++ WEB-UI/Controllers/PacienteController.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WEB_UI.Models;
+using System;
+using System.Collections.Generic;
 
 namespace WEB_UI.Controllers
 {
     public class PacienteController : Controller
     {
         public IActionResult LandingPaciente()
+        {
+            return View();
+        }
+        public IActionResult Lista()
+        {
+            return View();
+        }
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        public IActionResult Detalles()
         {
             return View();
         }
---
+++ WEB-UI/Models/ExamenPersonal.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_UI.Models
+{
+    public class ExamenPersonal
+    {
+        public string Especialidad { get; set; }
+        public string TipoDeExamen { get; set; }
+        public string Fecha { get; set; }
+    }
+}
