0d98a769e8e00a41eccf5a78a0a75e90ba2bec24
kendra-svg <ksanchov@ucenfotec.ac.cr>
2024-04-13 12:16:46 -0600

    Cambios en controller de recuperar contrasena.

--- WEB-UI/Controllers/VerificaOTPController.cs
+++ WEB-UI/Controllers/VerificaOTPController.cs
@@ -2,6 +2,9 @@
 using Azure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using Microsoft.AspNetCore.Http;
+using DTO;
+using WEB_UI.Models;
 
 namespace WEB_UI.Controllers
 {
@@ -11,31 +14,32 @@ namespace WEB_UI.Controllers
         
         {
 
-            var correoDestinatario = HttpContext.Session.GetString("correo");
+            
             //var sendEmailController = new SendEmailController();
             //sendEmailController.SendEmail(new Email { To = correoDestinatario, Subject = "Prueba", Body = "Cuerpo de prueba" });
 
-            EnviarCorreo();
+            //EnviarCorreo();
 
             return View();
         
         }
 
-        public void EnviarCorreo()
-        {
-            string connectionString = "endpoint=https://simepci-email.unitedstates.communication.azure.com/;accesskey=2L2jaulXE00cBujlGXm+c7MsfVlTzbr7ZGx321XgdBZgGYicwso177C+2bZ2VCviCAWc8d+f1cxzcENl2/iL0g==";
 
-            EmailClient emailClient = new EmailClient(connectionString);
+        //public void EnviarCorreo()
+        //{
+        //    string connectionString = "endpoint=https://simepci-email.unitedstates.communication.azure.com/;accesskey=2L2jaulXE00cBujlGXm+c7MsfVlTzbr7ZGx321XgdBZgGYicwso177C+2bZ2VCviCAWc8d+f1cxzcENl2/iL0g==";
+
+        //    EmailClient emailClient = new EmailClient(connectionString);
 
 
-            EmailSendOperation emailSendOperation = emailClient.Send(
-                WaitUntil.Completed,
-                senderAddress: "DoNotReply@b40196c4-78dc-4f03-90b2-4b7ae03bd7b5.azurecomm.net",
-                recipientAddress: "kendrasvega@gmail.com",
-                subject: "Correo electrónico de prueba",
-                htmlContent: "<html><h1>Hola mundo por correo electrónico.</h1l></html>",
-                plainTextContent: "Hola mundo por correo electrónico.");
-        }
+        //    EmailSendOperation emailSendOperation = emailClient.Send(
+        //        WaitUntil.Completed,
+        //        senderAddress: "DoNotReply@b40196c4-78dc-4f03-90b2-4b7ae03bd7b5.azurecomm.net",
+        //        recipientAddress: HttpContext.Session.GetString("Correo"),
+        //        subject: "Correo electrónico de prueba",
+        //        htmlContent: "<html><h1>Hola mundo por correo electrónico.</h1l></html>",
+        //        plainTextContent: "Hola mundo por correo electrónico.");
+        //}
     }
         
         
