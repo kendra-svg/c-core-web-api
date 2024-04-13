1ab1eb7f14265d9a6a66152370ab36d55ca15270
kendra-svg <ksanchov@ucenfotec.ac.cr>
2024-04-13 03:17:25 -0600

    Implementacion inicial de envio de correo electronico. Aun falta enviar generacion de otp y que dure un minuto

--- WEB-UI/wwwroot/js/VerificaOTP/VerificaOTP.js
+++ WEB-UI/wwwroot/js/VerificaOTP/VerificaOTP.js
@@ -1,1 +1,10 @@
-﻿
+﻿function setCorreoValue() {
+    $("#Correo").val(sessionStorage.getItem("correo"));
+    
+}
+
+
+$(document).ready(function () {
+    console.log(sessionStorage.getItem("correo"));
+    setCorreoValue();
+});
