a6deb9192dc58734ee19af39d8151da6b5319f49
kendra-svg <ksanchov@ucenfotec.ac.cr>
2024-04-14 20:40:35 -0600

    Implementacion de envio de correos  a la hora de registrarse. Pendiente de hacer tests, pero se requiere el api actualizado con el codigo

---
+++ AppLogic/AdminEmail.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Communication.Email;
+
+namespace AppLogic
+{
+    public class AdminEmail
+    {
+        public async Task<string> SendEmail(string emailAddress) // DTO Email 
+        {
+            string connectionString = "endpoint=https://simepci-email.unitedstates.communication.azure.com/;accesskey=2L2jaulXE00cBujlGXm+c7MsfVlTzbr7ZGx321XgdBZgGYicwso177C+2bZ2VCviCAWc8d+f1cxzcENl2/iL0g==";
+
+            EmailClient emailClient = new EmailClient(connectionString);
+
+            EmailContent emailContent = new EmailContent("Queremos agradecerle por reservar su cita"); // Subject
+            emailContent.PlainText = "Hemos recibido su solicitud de cita, y nos pondremos en contacto con usted";
+
+            List<EmailAddress> emailAddresses = new List<EmailAddress> { new EmailAddress(emailAddress, "Suscriptor de Cita") };
+            EmailRecipients emailRecipients = new EmailRecipients(emailAddresses);
+
+            EmailMessage emailMessage = new EmailMessage("DoNotReply@b40196c4-78dc-4f03-90b2-4b7ae03bd7b5.azurecomm.net", emailRecipients, emailContent);
+
+            EmailSendOperation emailSendOperation = await emailClient.SendAsync(
+                Azure.WaitUntil.Completed,
+                emailMessage, CancellationToken.None);
+
+
+            EmailSendResult statusMonitor = emailSendOperation.Value;
+
+            Console.WriteLine($"Email sent with status {emailSendOperation.Value.Status}");
+
+            return emailSendOperation.Value.Status.ToString();
+
+        }
+
+    }
+
+          
+
+        
+}
