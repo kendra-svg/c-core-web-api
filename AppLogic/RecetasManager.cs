48c5a0070f7fac591c0d98729166b7b1a4e55d5d
Vanessa <vvillegast@ucenfotec.ac.cr>
2024-04-18 13:11:11 -0600

    avance en inserts de receta

---
+++ AppLogic/RecetasManager.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Crud;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogic
+{
+    public class RecetasManager
+    {
+        public string CreateReceta(Receta rec)
+        {
+            RecetaCrud crud = new RecetaCrud();
+            crud.Create(rec);
+            return "ok";
+
+        }
+
+        public List<Receta> GetAllRecetas()
+        {
+            RecetaCrud crud = new RecetaCrud();
+            return crud.RetrieveAll<Receta>();
+        }
+    }
+}
