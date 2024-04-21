91c40ef035d10159c215f08447efffdada802c4f
kendra-svg <ksanchov@ucenfotec.ac.cr>
2024-04-20 20:46:08 -0600

    Cambios en DetallesSedes(para registro funcionarios en sedes)
    De momento ya se muestran las sedes, queda pendiente mostrar las especialidades relacionadas a esa sede al dar doble click, eso se va a mostrar en un nuevo grid
    
    se va a agregar un boton a dicho grid para agregar especialidad a esa sede
    
    se debe agregar un grid más, que traiga los funcionarios que pertenecen a esa sede, agregar un boton y poder editar y registrar nuevos a dicha sede
    
    Mapper no está terminado, sin embargo, el crud de insert a sede_especialidad ya fue creado, al igual que el get de especialidad by sede id

---
+++ DTO/SedeEspecialidad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class SedeEspecialidad: BaseClass
+    {
+
+        public SedeEspecialidad()
+        {
+            this.Sede = new List<Sede>();
+            this.Especialidad = new List<Especialidad>();
+            this.Usuario = new List<UsuarioBase>();
+        }
+        public List<Sede> Sede { get; set; }
+        public List<Especialidad> Especialidad { get; set; }
+        public List<UsuarioBase> Usuario { get; set; }
+
+    }
+}
---
+++ DataAccess/Mappers/SedeEspecialidadMapper.cs
@@ -0,0 +1,51 @@
+﻿using DataAccess.DAO;
+using DataAccess.Mappers.Interfaces;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Mappers
+{
+    public class SedeEspecialidadMapper : IObjectMapper, ICrudStatements
+    {
+        public BaseClass BuildObject(Dictionary<string, object> row)
+        {
+            SedeEspecialidad sedeEspecialidad = new SedeEspecialidad();
+            sedeEspecialidad.Id = int.Parse(row["id_sede"].ToString());
+            sedeEspecialidad.Especialidad =
+        }
+
+        public List<BaseClass> BuildObjects(List<Dictionary<string, object>> rowList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetCreateStatement(BaseClass dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetDeleteStatement(BaseClass dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetRetrieveAllStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetRetrieveByIdStatement(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetUpdateStatement(BaseClass dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
